627ae540cc696aeb99d76b67a5b0474fb3e093de
NadmorskaAlpaka <grzesio26500@gmail.com>
2023-07-25 11:47:28 +0200

    First structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
-import {faFaceLaugh} from '@fortawesome/free-solid-svg-icons';
+import Home from './pages/Home';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
  
 const App = () => {
   return (
     <>
-      <h1>Onyx Fitness</h1>
-      <FontAwesomeIcon icon={faDumbbell} />
-      <FontAwesomeIcon icon={faFaceLaugh} />
-      <p>Testuje gita</p>
+      <Home />
     </>
   )
 }
