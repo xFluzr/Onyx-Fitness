693a2c963268ad1a3c51b9bdcd8270f3569b9540
NadmorskaAlpaka <grzesio26500@gmail.com>
2023-08-02 13:11:20 +0200

    Structure of OurCoaches component and created API file for coaches data

---
+++ src/utils/coachesData.js
@@ -0,0 +1,104 @@
+import face1 from "../assets/coaches/face1.jpg";
+import face2 from "../assets/coaches/face2.jpg";
+import face3 from "../assets/coaches/face3.jpg";
+import face4 from "../assets/coaches/face4.jpg";
+import face5 from "../assets/coaches/face5.jpg";
+import face6 from "../assets/coaches/face6.jpg";
+import face7 from "../assets/coaches/face7.jpg";
+import face8 from "../assets/coaches/face8.jpg";
+import face9 from "../assets/coaches/face9.jpg";
+import face10 from "../assets/coaches/face10.jpg";
+
+import trener1 from "../assets/coaches/trener1.jpg";
+import trener2 from "../assets/coaches/trener2.jpg";
+import trener3 from "../assets/coaches/trener3.jpg";
+import trener4 from "../assets/coaches/trener4.png";
+import trener5 from "../assets/coaches/trener5.jpg";
+import trener6 from "../assets/coaches/trener6.jpg";
+import trener7 from "../assets/coaches/trener7.png";
+import trener8 from "../assets/coaches/trener8.jpg";
+import trener9 from "../assets/coaches/trener9.jpg";
+import trener10 from "../assets/coaches/trener10.jpg";
+
+export const coachesData = [
+    {
+        name: "Chris Bumstead",
+        discipline: "Bodybuilding",
+        icon: face10,
+        image: trener10,
+        description: "",
+        link: "chris_bumstead" 
+    },
+    {
+        name: "Michał Sakowski",
+        discipline: "Bodybuilding",
+        icon: face9,
+        image: trener9,
+        description: "",
+        link: "michal_sakowski" 
+    },
+    {
+        name: "Anna Lewandowska",
+        discipline: "Fitness",
+        icon: face7,
+        image: trener7,
+        description: "",
+        link: "anna_lewandowska" 
+    },
+    {
+        name: "Noel Deyzel",
+        discipline: "Bodybuilding",
+        icon: face8,
+        image: trener8,
+        description: "",
+        link: "noel_deyzel" 
+    },
+    {
+        name: "Andrea Larosa",
+        discipline: "Gymnastics",
+        icon: face6,
+        image: trener6,
+        description: "",
+        link: "andrea_larosa" 
+    },
+    {
+        name: "Irene Herman",
+        discipline: "Yoga",
+        icon: face1,
+        image: trener1,
+        description: "",
+        link: "irene_herman" 
+    },
+    {
+        name: "Emma Tereza",
+        discipline: "Yoga",
+        icon: face3,
+        image: trener3,
+        description: "",
+        link: "emma_tereza" 
+    },
+    {
+        name: "Kiana Misko",
+        discipline: "Martial arts",
+        icon: face4,
+        image: trener4,
+        description: "",
+        link: "kiana_misko" 
+    },
+    {
+        name: "Mamed Chalidow",
+        discipline: "Martial arts",
+        icon: face5,
+        image: trener5,
+        description: "",
+        link: "mamed_chalidow" 
+    },
+    {
+        name: "Marcin Najman",
+        discipline: "Boxer",
+        icon: face2,
+        image: trener2,
+        description: "",
+        link: "marcin_najman" 
+    }
+]
