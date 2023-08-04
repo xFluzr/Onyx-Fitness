589b3fa5eecf9db9b06ece584d9c899b83d20abf
NadmorskaAlpaka <grzesio26500@gmail.com>
2023-08-04 17:34:47 +0200

    Footer finished

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -12,62 +12,64 @@ const Footer = () => {
                 <div className="footer__wrapper">
                     <div className="footer__top">
                         <figure className="footer__img--wrapper">
-                            <img src={Logo} className="footer__img" alt="Onyx Fitness" />
+                            <Link className="footer__img--link" to="/">
+                                <img src={Logo} className="footer__img" alt="Onyx Fitness" />
+                            </Link>
                         </figure>
                         <ul className="footer__list">
                             <li className="footer__list--element">
-                                <Link to="" className="footer__list--link">
+                                <Link to="/excercises" className="footer__list--link">
                                     Excercises
                                 </Link>
                             </li>
                             <li className="footer__list--element">
-                                <Link to="" className="footer__list--link">
+                                <Link to="/pricing" className="footer__list--link">
                                     Pricing
                                 </Link>
                             </li>
                             <li className="footer__list--element">
-                                <Link to="" className="footer__list--link">
+                                <Link to="/bmi" className="footer__list--link">
                                     Bmi
                                 </Link>
                             </li>
                             <li className="footer__list--element">
-                                <Link to="" className="footer__list--link">
+                                <Link to="/contact" className="footer__list--link">
                                     Contact
                                 </Link>
                             </li>
                         </ul>
-                        <ul className="footer__list">
+                        <ul className="footer__list list2">
                             <li className="footer__list--element">
-                                <Link to="" className="footer__list--link">
+                                <Link to="/about" className="footer__list--link">
                                     About us
                                 </Link>
                             </li>
                             <li className="footer__list--element">
-                                <Link to="" className="footer__list--link">
+                                <Link to="/workouts" className="footer__list--link">
                                     Workouts
                                 </Link>
                             </li>
                             <li className="footer__list--element">
-                                <Link to="" className="footer__list--link">
-                                    Gallery
-                                </Link>
-                            </li>
-                            <li className="footer__list--element">
-                                <Link to="" className="footer__list--link">
+                                <Link to="/login" className="footer__list--link">
                                     Log in
                                 </Link>
                             </li>
+                            <li className="footer__list--element">
+                                <Link to="/gallery" className="footer__list--link">
+                                    Gallery
+                                </Link>
+                            </li>
                         </ul>
-                    </div>
-                    <div className="footer__middle">
-                        <FontAwesomeIcon className="coach__social-icon" icon={faInstagram} />
-                        <FontAwesomeIcon className="coach__social-icon" icon={faTwitter} />
-                        <FontAwesomeIcon className="coach__social-icon" icon={faFacebook} />
-                        <FontAwesomeIcon className="coach__social-icon" icon={faTiktok} />
+                        <div className="footer__icons">
+                            <FontAwesomeIcon className="footer__social-icon not-allowed" icon={faInstagram} />
+                            <FontAwesomeIcon className="footer__social-icon not-allowed" icon={faTwitter} />
+                            <FontAwesomeIcon className="footer__social-icon not-allowed" icon={faFacebook} />
+                            <FontAwesomeIcon className="footer__social-icon not-allowed" icon={faTiktok} />
+                        </div>
                     </div>
                     <div className="footer__bottom">
                         <p className="footer__text">
-                            This is not a real website.
+                            This is not a real gym website.
                         </p>
                         <p className="footer__text">
                             Created only for learning purposes.
