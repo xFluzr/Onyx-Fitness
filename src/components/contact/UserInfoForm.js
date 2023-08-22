95d6ab73ec41942358d4352ea277814256a7fc2f
Kuba <jakub.jaszcz538@gmail.com>
2023-08-22 11:50:38 +0200

    Contact Form Functionality added

--- src/components/contact/UserInfoForm.js
+++ src/components/contact/UserInfoForm.js
@@ -1,17 +1,25 @@
+import React,{ useRef } from 'react';
+import { sendEmail } from '../../utils/sendMail';
 import './userInfoForm.css';
 
+
 const UserInfoForm = () => {
-
-  const submitHanlder=(e)=>{
+  const form=useRef()
+  
+  const submitHandler=(e)=>{
     e.preventDefault();
-  }
+    sendEmail(process.env.REACT_APP_SERVICE_KEY,process.env.REACT_APP_CONTACT_FORM_TEMPLATE_KEY,form.current,process.env.REACT_APP_PUBLIC_KEY)
 
+    /*Cleaning input */
+    e.target.reset()
+  }
+ 
   return (
-    <form className='userInfo-form__wrapper' onSubmit={submitHanlder}>
+    <form ref={form} className='userInfo-form__wrapper' onSubmit={submitHandler}>
         <h3 className='form__title'>Contact us</h3>
-        <input className='userInfo-form__input' type='text' placeholder='Full Name*' required={true}/>
-        <input className='userInfo-form__input' type='email' placeholder='Email*' required={true}/>
-        <textarea className='userInfo-form__input' placeholder='Message'></textarea>
+        <input className='userInfo-form__input' type='text' name='full_name' placeholder='Full Name*' required={true}/>
+        <input className='userInfo-form__input' type='email' name='user_email' placeholder='Email*' required={true}/>
+        <textarea className='userInfo-form__input' name='user_message' placeholder='Message'></textarea>
         <button className="btn background" type={"submit"}>
             Submit now
       </button>
