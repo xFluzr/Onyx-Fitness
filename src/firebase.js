21ad2aac943208fc893706ce7a39a8e0320bff68
NadmorskaAlpaka <grzesio26500@gmail.com>
2023-08-28 22:42:14 +0200

    register update

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCt4AOXuZUEYJtn3UYGSPjgyK-vc-oRaos",
-  authDomain: "onyxfitness-bfe80.firebaseapp.com",
-  projectId: "onyxfitness-bfe80",
-  storageBucket: "onyxfitness-bfe80.appspot.com",
-  messagingSenderId: "596518814098",
-  appId: "1:596518814098:web:54cdb0b850524f1944014b",
-  measurementId: "G-VCKXZXY7D6"
+  apiKey: `${process.env.REACT_APP_FIREBASE_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
+  measurementId: `${process.env.REACT_APP_MESUREMENT_ID}`
 };
 
 const app = initializeApp(firebaseConfig);
